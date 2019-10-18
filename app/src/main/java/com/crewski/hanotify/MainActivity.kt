03fd456e12f3fcf0b22dc3e26fbdc81a92f3ea35
geeak <49695843+geeak@users.noreply.github.com>
2019-10-18 21:32:26 +0200

    Indent setOnClickListener declaration (whitespace only)

--- app/src/main/java/com/crewski/hanotify/MainActivity.kt
+++ app/src/main/java/com/crewski/hanotify/MainActivity.kt
@@ -26,43 +26,43 @@ class MainActivity : AppCompatActivity() {
         val api_password = sharedPref.getString(getString(R.string.api_password), "")
         ha_passwrod.setText(api_password)
 
-    register_btn.setOnClickListener{
-        var url = ha_url.text.toString()
-        val password = ha_passwrod.text.toString()
+        register_btn.setOnClickListener{
+            var url = ha_url.text.toString()
+            val password = ha_passwrod.text.toString()
 
-        if (!URLUtil.isValidUrl(url)){
-            Toast.makeText(this, "Invalid URL", Toast.LENGTH_SHORT).show()
-            return@setOnClickListener
-        }
-        url = url.removeSuffix("/")
-
-        with (sharedPref.edit()) {
-            putString(getString(R.string.home_assistant_url), url)
-            putString(getString(R.string.api_password), password)
-            commit()
-        }
-        val token = FirebaseInstanceId.getInstance().token
-
-        val dataJSON = JSONObject()
-        dataJSON.put("token", token)
-        val que = Volley.newRequestQueue(this)
-        val url_suffix = getString(R.string.url_suffix)
-        val req = object : JsonObjectRequest(Request.Method.POST, "$url$url_suffix", dataJSON,
-                Response.Listener { response ->
-                    val resJSON = JSONObject(response.toString())
-                    Toast.makeText(this, resJSON.getString("message"), Toast.LENGTH_SHORT).show()
-
-                }, Response.ErrorListener { error ->  Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
-
-        }) {
-            @Throws(AuthFailureError::class)
-            override fun getHeaders(): Map<String, String> {
-                val headers = HashMap<String, String>()
-                headers.put("Content-Type", "application/json");
-                headers.put("x-ha-access", password)
-                return headers
+            if (!URLUtil.isValidUrl(url)){
+                Toast.makeText(this, "Invalid URL", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
             }
-        }
+            url = url.removeSuffix("/")
+
+            with (sharedPref.edit()) {
+                putString(getString(R.string.home_assistant_url), url)
+                putString(getString(R.string.api_password), password)
+                commit()
+            }
+            val token = FirebaseInstanceId.getInstance().token
+
+            val dataJSON = JSONObject()
+            dataJSON.put("token", token)
+            val que = Volley.newRequestQueue(this)
+            val url_suffix = getString(R.string.url_suffix)
+            val req = object : JsonObjectRequest(Request.Method.POST, "$url$url_suffix", dataJSON,
+                    Response.Listener { response ->
+                        val resJSON = JSONObject(response.toString())
+                        Toast.makeText(this, resJSON.getString("message"), Toast.LENGTH_SHORT).show()
+
+                    }, Response.ErrorListener { error ->  Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
+
+            }) {
+                @Throws(AuthFailureError::class)
+                override fun getHeaders(): Map<String, String> {
+                    val headers = HashMap<String, String>()
+                    headers.put("Content-Type", "application/json");
+                    headers.put("x-ha-access", password)
+                    return headers
+                }
+            }
 
         que.add(req)
     }
